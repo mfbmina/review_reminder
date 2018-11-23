4d352da418c9c17d31b184f76ccdcc04130b9893
Matheus Mina <mfbmina@gmail.com>
2018-11-23 11:45:15 +0000

    Change production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: "review-reminder.herokuapp.com" }
+  config.action_controller.default_url_options = { host: "review-reminder.herokuapp.com" }
 end
